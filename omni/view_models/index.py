cb72c59c4cbfd1b88a9cde99305ff0e9021e293a
Robert <trungthin.luong@trueplus.vn>
2017-09-24 22:26:33 +0700

    import views

--- omni/view_models/index.py
+++ omni/view_models/index.py
@@ -2,9 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import ListView
-from django.views.generic.basic import TemplateView
+from django.views import generic
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(generic.basic.TemplateView):
     template_name = 'index.html'
