f269d2f6c498b593174b537c85d9e408f9baabc2
Robert <trungthin.luong@trueplus.vn>
2017-09-25 01:58:45 +0700

    import views

--- omni/urls.py
+++ omni/urls.py
@@ -5,7 +5,6 @@ from .views import index, site, template, login
 app_name = 'omni'
 urlpatterns = [
     url(r'^$', index.View.as_view(), name='index'),
-    url(r'^login/$', login.view, name='login'),
     url(r'^sites/$', site.View.as_view(), name='sites'),
     url(r'^sites/(?P<pk>[0-9]+)/$', site.detail, name='site_edit'),
     url(r'^templates/$', template.View.as_view(), name='templates'),
