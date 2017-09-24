dfe02afbe743c8a409eceb409a736f252ec304cb
Robert <trungthin.luong@trueplus.vn>
2017-09-24 22:03:47 +0700

    import views

--- omni/urls.py
+++ omni/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from . import views
 
 app_name = 'omni'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^sites/$', views.Sites.as_view()),
-    url(r'^sites/(?P<pk>[0-9]+)/$', view_models.site.detail),
-    url(r'^templates/$', views.Templates.as_view()),
-    url(r'^templates/(?P<pk>[0-9]+)/$', view_models.templates.detail),
+    url(r'^sites/$', views.Sites.as_view(), name='sites'),
+    url(r'^sites/(?P<pk>[0-9]+)/$', view_models.site.detail, name='site_edit'),
+    url(r'^templates/$', views.Templates.as_view(), name='templates'),
+    url(r'^templates/(?P<pk>[0-9]+)/$', view_models.templates.detail, name='template_edit'),
 ]
