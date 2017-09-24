3010d1ced6a99d8fd46eb8f94e556e76d4424db8
Robert <trungthin.luong@trueplus.vn>
2017-09-24 21:07:55 +0700

    add urls and models

--- omni/models.py
+++ omni/models.py
@@ -11,6 +11,33 @@ class Extension(Model):
     repo_url = models.CharField(max_length=150)
     branch = models.CharField(max_length=30, default='master')
 
+class Site(Model):
+    id = models.IntegerField(default=0)
+    url_path = models.CharField(max_length=30)
+    admin_user = models.CharField(max_length=30)
+    admin_password = models.CharField(max_length=60)
+    db_name = models.CharField(max_length=100)
+    db_user = models.CharField(max_length=100)
+    db_password = models.CharField(max_length=60)
+    framework = models.CharField(max_length=200)
+    image = models.CharField(max_length=100)
+    state = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
+
+class Template(Model):
+    id = models.IntegerField(default=0)
+    admin_user = models.CharField(max_length=30)
+    admin_password = models.CharField(max_length=60)
+    db_name = models.CharField(max_length=100)
+    db_user = models.CharField(max_length=100)
+    db_password = models.CharField(max_length=60)
+    framework = models.CharField(max_length=200)
+    from_site = models.CharField(max_length=200)
+    image = models.CharField(max_length=100)
+    state = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+
 class TemplateExtension(Model):
     id = models.IntegerField(default=0)
     extension_id = models.ForeignKey(Extension, on_delete=models.CASCADE)
@@ -20,35 +47,3 @@ class SiteExtension(Model):
     id = models.IntegerField(default=0)
     extension_id = models.ForeignKey(Extension, on_delete=models.CASCADE)
     template_id = models.ForeignKey(Site, on_delete=models.CASCADE)
-
-
-class Site(Model):
-    id = models.IntegerField(default=0)
-    url_path = models.CharField(max_length=30)
-    admin_user = models.CharField(max_length=30)
-    admin_password = models.CharField(max_length=60)
-    db_name = models.CharField(max_length=100)
-    db_user = models.CharField(max_length=100)
-    db_password = models.CharField(max_length=60)
-    extensions = models.ForeignKey(SiteExtension, on_delete=models.CASCADE)
-    framework = models.CharField(max_length=200)
-    image = models.CharField(max_length=100)
-    state = models.CharField(max_length=30)
-    title = models.CharField(max_length=50)
-
-
-class Template(Model):
-    id = models.IntegerField(default=0)
-    admin_user = models.CharField(max_length=30)
-    admin_password = models.CharField(max_length=60)
-    db_name = models.CharField(max_length=100)
-    db_user = models.CharField(max_length=100)
-    db_password = models.CharField(max_length=60)
-    extensions = models.ForeignKey(TemplateExtension, on_delete=models.CASCADE)
-    framework = models.CharField(max_length=200)
-    from_site = models.CharField(max_length=200)
-    image = models.CharField(max_length=100)
-    state = models.CharField(max_length=30)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
-
