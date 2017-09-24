a80a4d7d6c8367234dfa95f0e751bb786a8ef804
Robert <trungthin.luong@trueplus.vn>
2017-09-24 23:14:53 +0700

    import views

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
-    url(r'^/', include(admin.site.urls)),
-    url(r'^omni/', include('omni.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('omni.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': reverse_lazy('index')}, name='login'),
 ]
