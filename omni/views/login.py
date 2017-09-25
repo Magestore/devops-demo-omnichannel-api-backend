90310d5f0489d78ff9c112721612eba3fe1c5431
Robert <trungthin.luong@trueplus.vn>
2017-09-25 09:05:57 +0700

    import views

--- omni/views/login.py
+++ omni/views/login.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import View
+from omni.models import user
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
@@ -13,4 +14,12 @@ class LoginView(View):
     def post(self, request, *args, **kwargs):
         u = request.POST['username']
         p = request.POST['password']
+
+        return args
+
+        if user.mylogin(request):
+
+            return redirect(reverse('login', args=['next=' + request.path]))
+        else:
+            return redirect(reverse('login', args=['next='+request.path]))
         return render(request, 'login_post.html', {'user': {'u': u, 'p': p}})
