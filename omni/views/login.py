88464536b1d1ebb04519dca9cc5a6efac1a7214f
Robert <trungthin.luong@trueplus.vn>
2017-09-25 08:01:01 +0700

    import views

--- omni/views/login.py
+++ omni/views/login.py
@@ -6,15 +6,11 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import View
 
-# Create your views here.
-def view(request):
-    u = 'default' # request.GET['username']
-    p = 'default' # request.GET['password']
-    return render(request, 'login.html', {'user': {'u': u, 'p': p}})
-
 class LoginView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('This is GET request')
+        return render(request, 'login.html', {'user': {'u': u, 'p': p}})
 
     def post(self, request, *args, **kwargs):
-        return HttpResponse('This is POST request')
+        u = request.GET['username']
+        p = request.GET['password']
+        return render(request, 'login.html', {'user': {'u': u, 'p': p}})
