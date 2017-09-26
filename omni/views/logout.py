682e6e3c2964063caeb204c8bb0d5ba213be113c
Robert <trungthin.luong@trueplus.vn>
2017-09-26 07:50:53 +0700

    import template layout html

--- omni/views/logout.py
+++ omni/views/logout.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 
 from omni.models import user
 
 # Create your views here.
 def view(request):
-    return user.mylogout(request)
+    user.mylogout(request)
+    return redirect('logout')
