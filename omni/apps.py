c5587f9786799a3fd01da4ab0cf9bac2897dec97
Robert <trungthin.luong@trueplus.vn>
2017-11-10 08:51:12 +0700

    add context menu to view

--- omni/apps.py
+++ omni/apps.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-
+from omni.models.menu import Menu
 
 class OmniConfig(AppConfig):
     name = 'omni'
+
+## Global menu variable
+menu = Menu()
--- omni/custom_context.py
+++ omni/custom_context.py
@@ -4,9 +4,6 @@ from __future__ import unicode_literals
 from django.template import RequestContext
 from omni.models.menu import Menu
 
-## Global menu variable
-menu = Menu()
-
 def menu_context(request):
     global menu
     return {'menu': menu}
